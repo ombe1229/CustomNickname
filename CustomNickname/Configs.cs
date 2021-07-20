336966bb63fd9f5b62f7d4f15d1a21e94c1d0045
ombe <h3236516@gmail.com>
2021-07-20 17:20:43 +0900

    feat(configs): create configs.cs

---
+++ CustomNickname/Configs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace CustomNickname
+{
+    public class Configs : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("커스텀 닉네임 설정")]
+        public Dictionary<string, string> CustomDictionary { get; private set; } = new Dictionary<string, string>
+        {
+            
+        };
+    }
+}
