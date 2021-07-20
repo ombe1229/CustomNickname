2cbda40b856535185cc93f28bd7aded3b61e3d7d
ombe <h3236516@gmail.com>
2021-07-20 16:41:48 +0900

    feat(plugin): create Plugin.cs

---
+++ CustomNickname/EventHandlers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomNickname
+{
+    public class EventHandlers : IDisposable
+    {
+        private Plugin _pluginInstance;
+        public EventHandlers(Plugin pluginInstance) => _pluginInstance = pluginInstance;
+
+        public void Dispose()
+        {
+            _pluginInstance = null;
+        }
+    }
+}
